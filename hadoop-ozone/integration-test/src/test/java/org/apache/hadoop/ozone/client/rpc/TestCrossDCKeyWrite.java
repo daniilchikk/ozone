0f4d721cce3113c36f7dd0010e43faafd44be3df
daniilchikk <dprizva23@gmail.com>
2025-03-21 18:06:53 +0300

    Fix TestCrossDCKeyWrite

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestCrossDCKeyWrite.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestCrossDCKeyWrite.java
@@ -19,8 +19,6 @@ package org.apache.hadoop.ozone.client.rpc;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.HashMap;
@@ -106,12 +104,7 @@ class TestCrossDCKeyWrite {
           List<String> dns = hddsDatanodes.stream()
                 .map(dn -> {
                   int ratisPort = Integer.parseInt(dn.getConf().get(HDDS_CONTAINER_RATIS_IPC_PORT));
-                  String host;
-                  try {
-                    host = InetAddress.getLocalHost().getHostAddress();
-                  } catch (UnknownHostException e) {
-                    throw new RuntimeException(e);
-                  }
+                  String host = "127.0.0.1";
                   return host + ":" + ratisPort;
                 })
                 .collect(Collectors.toList());
@@ -160,12 +153,7 @@ class TestCrossDCKeyWrite {
           List<String> dns = hddsDatanodes.stream()
                 .map(dn -> {
                   int ratisPort = Integer.parseInt(dn.getConf().get(HDDS_CONTAINER_RATIS_IPC_PORT));
-                  String host;
-                  try {
-                    host = InetAddress.getLocalHost().getHostAddress();
-                  } catch (UnknownHostException e) {
-                    throw new RuntimeException(e);
-                  }
+                  String host = "127.0.0.1";
                   return host + ":" + ratisPort;
                 })
                 .collect(Collectors.toList());
