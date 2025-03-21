c1e8e67bab4f1ad42d103009b15fcd61e584958b
daniilchikk <dprizva23@gmail.com>
2025-03-21 16:36:35 +0300

    Fix TestCrossDCKeyWrite

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestCrossDCKeyWrite.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestCrossDCKeyWrite.java
@@ -106,13 +106,13 @@ class TestCrossDCKeyWrite {
           List<String> dns = hddsDatanodes.stream()
                 .map(dn -> {
                   int ratisPort = Integer.parseInt(dn.getConf().get(HDDS_CONTAINER_RATIS_IPC_PORT));
-                    String host;
-                    try {
-                        host = InetAddress.getLocalHost().getHostAddress();
-                    } catch (UnknownHostException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return host + ":" + ratisPort;
+                  String host;
+                  try {
+                    host = InetAddress.getLocalHost().getHostAddress();
+                  } catch (UnknownHostException e) {
+                    throw new RuntimeException(e);
+                  }
+                  return host + ":" + ratisPort;
                 })
                 .collect(Collectors.toList());
 
